23df39c91db8578aedb8d6a0c0429cb22268340c
vr-varad <varadgupta21#gmail.com>
2024-05-12 12:59:03 +0530

    Completed Firebase CRUD

--- firebase/src/config/firebase.js
+++ firebase/src/config/firebase.js
@@ -9,13 +9,13 @@ import {getStorage} from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzK7ueq-FK304571VB8QZtHlYWCnost_w",
-  authDomain: "fir-course-6657d.firebaseapp.com",
-  projectId: "fir-course-6657d",
-  storageBucket: "fir-course-6657d.appspot.com",
-  messagingSenderId: "74066038295",
-  appId: "1:74066038295:web:904d7a4b314127a0f3d652",
-  measurementId: "G-1HB900VJ6X"
+  apiKey: "###########`",
+  authDomain: "888DRGHSRTTHSRTH",
+  projectId: "WETGETTHWETTH",
+  storageBucket: "SEGHSEETHSETTH",
+  messagingSenderId: "7WRTHRTH5",
+  appId: "1:74066038295:WRTHWE5HSTH14127a0f3d652",
+  measurementId: "GSRRHSHB900VJ6X"
 };
 
 // Initialize Firebase
